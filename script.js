fa99bcba41a698c4b19d9a795f0114f9748358c2
murari2401 <iammurari89@gmail.com>
2024-03-01 02:24:00 +0530

    Calculation score and timer setting

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var timer=60;
+var score=0;
+var hitrn=0;
+function increaseScore(){
+    score+=10;
+    document.querySelector("#scoreVal").textContent=score;
+}
+
 function makeBubble(){
     var clutter="";
     for(var i=1;i<=168+16;i++)
@@ -9,10 +17,9 @@ function makeBubble(){
 }
 
 function getNewHit(){
-    var rn=Math.floor(Math.random()*10);
-    document.querySelector("#hitVal").textContent=rn;
+    hitrn=Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent=hitrn;
 }
-var timer=60;
 function runTimer(){
     var timerInterval = setInterval(function(){
         if(timer>0){
@@ -20,10 +27,22 @@ function runTimer(){
             document.querySelector("#timerValue").textContent=timer;
         }
         else{
-            clearInterval(timer);
+            clearInterval(timerInterval);
+            document.querySelector("#pbtm").innerHTML=`<h1>Game Over. <br> Your Score is ${score}</h1>`;
         }
     },1000);
 }
+
+document.querySelector("#pbtm").addEventListener("click",function(dets){
+    // console.log(dets.target.textContent);
+    var clickerNumner= Number(dets.target.textContent);
+    if(clickerNumner==hitrn)
+    {
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
 
 runTimer();
 makeBubble();
